4e557ec6550519fc6dc121041cc491808027cccf
forEngineer-hub <70783654+forEngineer-hub@users.noreply.github.com>
2021-07-13 21:04:13 +0900

    x

---
+++ src/components/ToDos/actions/index.tsx
@@ -0,0 +1,59 @@
+import { NONAME } from "dns";
+import { ToDo } from "../../../redux/todo/toDoReducer";
+
+export const ADD_TODO = "add_todo";
+export const SHOW_ALL = "all";
+export const SHOW_DONE = "done";
+export const SHOW_UNDONE = "undone";
+export const COMPELETE = "compelete";
+export const SET_FILTER = "SET_FILTER";
+
+export let idx: number = 1;
+
+interface AddTodoAction {
+  type: typeof ADD_TODO;
+  payload: ToDo | undefined;
+}
+
+export interface SetFilterAction {
+  type: typeof SET_FILTER;
+  payload: string;
+}
+
+//动态生成action
+export const setFilterActionCreator = (filter: string): SetFilterAction => {
+  return {
+    type: SET_FILTER,
+    payload: filter,
+  };
+};
+
+interface compelete {
+  type: typeof COMPELETE;
+  payload: number;
+}
+
+export type actionType = AddTodoAction;
+
+export const AddTodoActionCreator = (name: string): AddTodoAction => {
+  fetch("http://localhost:8081/searchHistory/getSearchHistory", {
+    mode: "cors",
+  })
+    .then((res) => {
+      console.log(res);
+      return res.json;
+    })
+    .then((data) => console.log(data));
+  debugger;
+  return {
+    type: ADD_TODO,
+    payload: { id: ++idx, name: name, status: "undone" },
+  };
+};
+
+export const CompeleteActionCreator = (index: number): compelete => {
+  return {
+    type: COMPELETE,
+    payload: index,
+  };
+};
